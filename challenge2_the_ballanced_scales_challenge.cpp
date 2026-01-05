2a70e37d06981f9eecc4a3a4a05da66c3ea5767c
Muhammad Talha <muhammadtalha.mailme@gmail.com>
2026-01-05 22:19:20 +0500

    Week 2 Progress: Solved Problem 2

---
+++ challenge2_the_ballanced_scales_challenge.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+Asymptotic Analysis
+
+Time Complexity = O(N * S)
+Space Complexity = O(N)
+
+*/
+
+
+bool can_balance_scales(vector<int>& arr) {
+    int s = accumulate(arr.begin(), arr.end(), 0);
+    if(s & 1) return false;
+
+    vector<bool> possibleSums(s / 2 + 1, false);
+
+    possibleSums[s / 2] = true;
+
+    sort(arr.rbegin(), arr.rend());
+
+    for(int i = 0; i < arr.size(); i++) {
+        for(int sm = arr[i]; sm <= s / 2; sm++) {
+            if(possibleSums[sm]) {
+                possibleSums[sm - arr[i]] = true;
+            }
+        }
+    }
+
+    return possibleSums[0];
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    vector<int> arr = {1, 3, 5};
+    cout << can_balance_scales(arr);
+
+    return 0;
+}
